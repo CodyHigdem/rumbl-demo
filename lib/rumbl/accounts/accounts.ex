502ecdcf6eeb449125ae26cd759d9a2a806e0f8f
Cody <meanderingmonarch@gmail.com>
2018-09-03 08:43:24 -0500

    workig on logging in

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -6,6 +6,7 @@ defmodule Rumbl.Accounts do
   alias Rumbl.Accounts.User
   alias Rumbl.Repo
 
+  import Ecto.Query
 
   def create_user(attrs \\ %{}) do
     %User{}
@@ -37,6 +38,25 @@ defmodule Rumbl.Accounts do
     %User{}
     |> User.registration_changeset(attrs)
     |> Repo.insert()
+  end
+
+  def get_user_by_email(email) do
+    from(u in User, join: c in assoc(u, :credential), where: c.email == ^email)
+    |> Repo.one()
+    |> Repo.preload(:credential)
+  end
+
+  def authenticate_by_email_and_pass(email, given_pass) do
+    user = get_user_by_email(email)
+
+    cond do
+      user && Comeonin.Pbkdf2.checkpw(given_pass, user.credential.password_hash) ->
+        {:ok, user}
+      user ->
+        {:error, :unauthorized}
+      true ->
+        Comeonin.Bcrypt.dummy_checkpw()
+        {:error, :not_found}
   end
 
   alias Rumbl.Accounts.Credential
